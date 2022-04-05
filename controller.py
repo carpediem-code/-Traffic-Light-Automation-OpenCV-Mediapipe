e9fe81da03f6987310c8f028e6328c193eee1bb2
Mitam samanta <77063697+carpediem-code@users.noreply.github.com>
2022-04-05 20:22:44 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,7 +7,6 @@ board=pyfirmata.Arduino(comport)
 led_1=board.get_pin('d:13:o')
 led_2=board.get_pin('d:12:o')
 led_3=board.get_pin('d:11:o')
-led_4=board.get_pin('d:10:o')
 
 def led(total):
     if total==0:
@@ -30,16 +29,6 @@ def led(total):
         led_2.write(0)
         led_3.write(0)
         led_4.write(1)
-    if total==4:
-        led_1.write(0)
-        led_2.write(0)
-        led_3.write(0)
-        led_4.write(0)
-    if total==5:
-        led_1.write(0)
-        led_2.write(0)
-        led_3.write(0)
-        led_4.write(0)
     
         
         
